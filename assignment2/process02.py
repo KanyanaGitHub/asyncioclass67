172dbdb6c28ad310c6a7da41e1e068a8cc56cd70
KanyanaGitHub <ktoadithep@gmail.com.com>
2024-06-28 09:43:15 +0700

    process2

--- assignment2/process02.py
+++ assignment2/process02.py
@@ -1,2 +1,21 @@
 # Multiprocessing 2 kitkens, 2 cooker, 2 dishes
 # share resources
+from time import sleep, ctime, time
+
+# Cooking synchonous
+def cooking(index) :
+    print(f'{ctime()} Kitchen-{index} : Begin cooking...')
+    sleep(2)
+    print(f'{ctime()} Kitchen-{index} : Cooking done!')
+    
+if __name__=="__main__" :
+    # Begin of main thread
+    print(f'{ctime()} Main : Start Cooking.')
+    start_time = time()
+    
+    # Cooking for each dish
+    for index in range(2):
+        cooking(index)
+    
+    duration = time() - start_time
+    print(f"{ctime()} Main : Finished Cooking duration in {duration:0.2f} second")
