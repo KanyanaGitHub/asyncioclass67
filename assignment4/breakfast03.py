907b51b8bc3b092f934b44c00adbcc2a3b34c2a8
KanyanaGitHub <ktoadithep@gmail.com.com>
2024-07-05 11:23:47 +0700

    add teacher

--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -16,10 +16,19 @@ async def fry_egg(): #1
     print('coffee: waitting......')
     await asyncio.sleep(3) # 2: pause, another tasks can be run
     print("eggs: ready")
-    
-async def main(): #1
+
+async def main(): #1 by teacher
     start = time()
-    await asyncio.gather(make_coffee(), fry_egg())
+    coffee_task = asyncio.create_task(make_coffee()) #shcedule execution
+    eggs_task = asyncio.create_task(fry_eggs()) #shcedule execution
+    #wait for completion, both tasks are corotine
+    await make_coffee #run task with await
+    await fry_egg
     print(f'breakfast is ready in {time()-start} min')
+    
+# async def main(): #1 gather
+#     start = time()
+#     await asyncio.gather(make_coffee(), fry_egg())
+#     print(f'breakfast is ready in {time()-start} min')
     
 asyncio.run(main()) # run top-level function
