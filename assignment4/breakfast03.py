fca7f488b653a056a6d6f42a52b483b7c5ed07db
KanyanaGitHub <ktoadithep@gmail.com.com>
2024-07-05 11:34:10 +0700

    toast_bread

--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -13,22 +13,31 @@ async def make_coffee(): #1
 async def fry_egg(): #1
     print("eggs: prepare ingridients")
     sleep(1)
-    print('coffee: waitting......')
+    print('eggs: waitting......')
     await asyncio.sleep(3) # 2: pause, another tasks can be run
     print("eggs: ready")
+    
+async def toast_bread(): #1
+    print("bread: prepare ingridients")
+    sleep(1)
+    print('bread: waitting......')
+    await asyncio.sleep(3) # 2: pause, another tasks can be run
+    print("bread: ready")
 
 async def main(): #1 by teacher
     start = time()
     coffee_task = asyncio.create_task(make_coffee()) #shcedule execution
-    eggs_task = asyncio.create_task(fry_eggs()) #shcedule execution
-    #wait for completion, both tasks are corotine
-    await make_coffee #run task with await
-    await fry_egg
+    egg_task = asyncio.create_task(fry_egg()) #shcedule execution
+    bread_task = asyncio.create_task(toast_bread()) #shcedule execution
+    #wait for completion, both tasks are coroutines
+    await coffee_task #run task with await
+    await egg_task
+    await bread_task
     print(f'breakfast is ready in {time()-start} min')
     
 # async def main(): #1 gather
 #     start = time()
-#     await asyncio.gather(make_coffee(), fry_egg())
+#     await asyncio.gather(make_coffee(), fry_egg(),  toast_bread())
 #     print(f'breakfast is ready in {time()-start} min')
     
 asyncio.run(main()) # run top-level function
